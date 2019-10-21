947fc0cb5182858058bf7d48d3746a0c52a195aa
Robert Rudolph <rudolph.consulting@gmail.com>
2019-10-21 20:28:04 +0200

    try git

--- README.md
+++ README.md
@@ -6,3 +6,25 @@
 [![Build Status](https://ci.appveyor.com/api/projects/status/github/rryi/Tokens.jl?svg=true)](https://ci.appveyor.com/project/rryi/Tokens-jl)
 [![Codecov](https://codecov.io/gh/rryi/Tokens.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/rryi/Tokens.jl)
 [![Build Status](https://api.cirrus-ci.com/github/rryi/Tokens.jl.svg)](https://cirrus-ci.com/github/rryi/Tokens.jl)
+
+Tokens.jl supplies tools to parse text into tokens, build memory-efficient vectors and trees of tokens, and work with mutable strings. It uses shared buffers, avoiding heap allocations for single tokens and tree elements.
+
+Inspirations came from Nicholas Ormrod's talk on [strange details of std::string at Facebook](https://www.youtube.com/watch?v=kPR8h4-qZdk), and  [ShortStrings](https://github.com/xiaodaigh/ShortStrings.jl) for interned strings. 
+
+# Basic structures
+
+## AbstractToken <: AbstractString
+A string with additionally attached token category (a value in 0..7). See token interface chapter.
+
+## TinyToken <: AbstractToken
+A token flyweight, of only 8 bytes. It consists of a token category (a value between 0 and 7) and text, either stored directly in the remaining 7 bytes, or stored in some external buffer.
+
+## Token <: AbstractToken
+holds a category ID  of non-heap storage
+. aims on  It also covers mutable strings general string processing tasks and a mutable string dwith mutable  and mutable strings.
+
+A token is a text fragment recognized to be of some token category. . perform string manipulation do string manipulation.
+
+ext fragments structures for parsing and manipulating text fragments which can have a category attached# Intention
+
+# AbstractToken interface
--- src/Tokens.jl
+++ src/Tokens.jl
@@ -1,5 +1,105 @@
 module Tokens
 
-greet() = print("Hello World!")
+const CU = UInt8 # currently, only support bytes as code units
+
+# abstract type AbstractToken{C} where {C<:Integer} <: AbstractString
+abstract type AbstractToken <: AbstractString
+end
+
+"""
+Flyweight string with an associated token category (0..7).
+
+interpreted as an Int64 in little endian format, the
+following bit fields are encoded:
+
+byte8, bit8: 0=direct string, 1=buffered string
+this is the sign bit of the Int64!
+It determines interpretation of the rest of TinyToken:
+
+## direct string: >=0 (byte8, bit8 ==0)
+byte8, bit1..3: length 0..7
+byte1 is the last code unit
+byte2 the 2nd last code unit
+...
+byte(8-s) the 1st code unit.
+if s<7, byte(s+1..7) are 0.
+
+This format allows to compare
+
+## buffered string: <0 (byte8, bit8 ==1)
+
+byte7, Bit0..2: length 0..7
+
+if direct string, the rest is interpreted as follows:
+byte0..byte6
+byte0
+
+"""
+primitive type TinyToken <: AbstractToken 64 end
+
+const maxTinySize = 7; # maximal size of a direct string in TinyToken
+
+const isUtf8 :: UInt8 = 8;
+
+mutable struct Token {
+    tiny :: TinyToken # current value, offset referencing buffer
+    buffer :: Vector{CU} # memory with token text data.
+    # TODO use C String ptr and limit index
+    offset :: UInt32 # value to add to 1 to get index of 1st CU in buffer
+    shared :: UInt32 #last index in buffer shared with other tokens
+} end
+
+
+function TinyToken(s::String, category::UInt8=0)
+    sz = sizeof(s)
+    # try to encode in 7 bytes. tinytoken
+    if sz < sizeof(::TinyToken)
+        str :: Int64 = 0
+        utf8Flag :: UInt8 = 0
+        i = 0
+        while ++i <= sz
+            c = codeunit(s,i)
+            (str *= 8) += c
+            if c>127
+            end
+
+        end
+        p :: Ptr{UInt8} = pointer(s)
+        (str *= 8) += codeunit(s,i)
+
+        str = (T(s |> pointer |> Ptr{TinyToken} |> Base.unsafe_load |> ntoh)
+    end
+    # check for UTF8 encoded ISO-8859-1 string: sizeof(s)>=8 could be valid in this case
+    throw(ErrorException("supplied string size $sz exceeds size limit for TinyToken - use Token, instead"))
+
+    bits_to_wipe = 8(sizeof(T) - sz)
+    content = (T(s |> pointer |> Ptr{TinyToken} |> Base.unsafe_load |> ntoh) >> bits_to_wipe) << bits_to_wipe
+    TinyToken{T}(content | T(sz))
+end
+
+String(s::TinyToken) = String(reinterpret(UInt8, [s.size_content|>ntoh])[1:sizeof(s)])
+
+Base.lastindex(s::TinyToken) = Int(s.size_content & 0xf)
+Base.iterate(s::TinyToken, i::Integer) = iterate(String(s), i)
+Base.iterate(s::TinyToken) = iterate(String(s))
+Base.sizeof(s::TinyToken) = Int(s.size_content & 0xf)
+Base.print(s::TinyToken) = print(String(s))
+Base.display(s::TinyToken) = display(String(s))
+Base.convert(::TinyToken{T}, s::String) where T = TinyToken{T}(s)
+Base.convert(::String, ss::TinyToken) = String(a) #reduce(*, ss)
+Base.firstindex(::TinyToken) = 1
+Base.ncodeunits(s::TinyToken) = ncodeunits(String(s))
+Base.codeunit(s::TinyToken, i) = codeunits(String(s), i)
+Base.isvalid(s::TinyToken, i::Integer) = isvalid(String(s), i)
+
+Base.getindex(s::TinyToken{T}, i::Integer) where T = begin
+    print(i)
+    Char((s.size_content << 8(i-1)) >> 8(sizeof(T)-1))
+end
+Base.collect(s::TinyToken) = getindex.(s, 1:lastindex(s))
+
+==(s::TinyToken, b::String) = begin
+    String(s)  == b
+end
 
 end # module
